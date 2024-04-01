517105f1a532a2fc7700b383a25dc3635eedc430
aribambang <aribk210@gmail.com>
2024-04-01 09:59:42 +0700

    install htmx

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 import express from 'express';
+import { programmingList } from './data/data.js';
 
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send(`<html><body>Hello World</body></html>`);
+  res.send(`
+  <html>
+    <head>
+      <script src="/htmx.js" defer></script>
+    </head>
+    <body>
+      <h1>Programming List</h1>
+      <button hx-get="/info" hx-swap="outerHTML">show</button>     
+    </body>
+  </html>`);
+});
+
+app.get('/info', (req, res) => {
+  res.send(`
+    <ul>
+      ${programmingList.map((programming) => `<li>${programming}</li>`)}
+    </ul>
+  `);
 });
 
 console.log('app running on http://localhost:3000');
---
+++ data/data.js
@@ -0,0 +1,1 @@
+export const programmingList = ['JavaScript', 'HTML', 'CSS'];
